3ef9b88a28011dc79af422d98e47bc1132075e8e
Gabriel <gabrielgranadosc@gmail.com>
2020-12-05 16:52:40 -0600

    Login Listo

---
+++ src/java/BaseDeDatos/Interts_Usuario_Departamento.sql
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  gabri
+ * Created: Dec 5, 2020
+ */
+    
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Gabriel");
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Pablo");
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Miguel");
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Christopher");
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Shamuel");
+INSERT INTO Departamento (nombreSistema) VALUES ("Sistema Emily");
+
+
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Pablo Porras", 2, "josexpablox@gmail.com", "pablo123");
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Miguel Soto", 3, "msoto@gmail.com", "miguel123");
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Christopher Valerio", 4, "chile@gmail.com", "chile123");
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Shamuel Simpson", 5, "shamuelsimpson@gmail.com", "shamuel123");
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Gabriel Granados", 1, "gabrielgranadosc@gmail.com", "gabriel123");
+INSERT INTO Usuario(nombre, idDepartamento, correo, contrasena) VALUES ("Emily Bolivar", 6, "emilyb@gmail.com", "emily123");
+
+
+
+
