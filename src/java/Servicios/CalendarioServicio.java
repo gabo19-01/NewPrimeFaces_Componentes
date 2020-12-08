f14f0bd13351ec7cefea12a1401aca9d2f38f595
Gabriel <gabrielgranadosc@gmail.com>
2020-12-08 08:58:52 -0600

    Inserts de Alerta e Insert de EVentos sin compra

--- src/java/Servicios/CalendarioServicio.java
+++ src/java/Servicios/CalendarioServicio.java
@@ -5,29 +5,31 @@
  */
 package Servicios;
 
-import Modelo.Evento;
+import correo.Alerta;
+import correo.Evento;
+;
+import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 /**
  *
  * @author gabri
  */
+
+
 public class CalendarioServicio {
+
+    private static EntityManager em = null;
     
     @Transactional
-    public void insertEvent(Evento evento){
-       /* entityManager.createNativeQuery("INSERT INTO Evento (tiempoInicio, tiempoFin, creador, titulo, descripcion, departamento, adjuntos, completado, Participantes, tipoAlerta) VALUES (?,?,?,?,?,?,?,?,?,?)")
-.setParameter(1, evento.getTiempoInicio())
-.setParameter(2, evento.getTiempoFin())
-.setParameter(3, evento.getCreador())
-.setParameter(4, evento.getTitulo())
-.setParameter(5, evento.getDescripcion())
-.setParameter(6, evento.getDepartamento())
-.setParameter(7, evento.getAdjuntos())
-.setParameter(8, evento.getCompletado())
-.setParameter(9, evento.getParticipantes())
-.setParameter(10, evento.getTipoAlerta())
-.executeUpdate();
-*/
+    public int insertEvent(Evento evento){
+        this.em.persist(evento);
+        em.flush();
+        return evento.getEventoID();
+    }
+    @Transactional
+    public void insertAlerta(Alerta alerta){
+        this.em.persist(alerta);
+        em.flush(); 
     }
 }
